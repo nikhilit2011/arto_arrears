4d2aaeb5e4fc53513c58c861b7e915a392665939
NIKHIL <nikhil@NIKHILs-MacBook-Air.local>
2025-08-27 14:57:01 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,23 @@ Rails.application.routes.draw do
       get  :sample_template   # ← tax-paid import template
     end
   end
+  
+  # --- TEMP ADMIN CREATOR ROUTE ---
+  get "/create_admin", to: proc { |env|
+    email    = "admin@arrear.com"          # <-- hardcoded
+    password = "Pass@12345"        # <-- hardcoded
+
+    user = User.find_or_initialize_by(email: email)
+    user.password = password
+    user.password_confirmation = password
+    user.role = "admin" if user.respond_to?(:role=)
+    user.save!
+
+    [
+      200,
+      { "Content-Type" => "text/plain" },
+      ["Admin created/updated. Email: #{email}, Password: #{password}"]
+    ]
+  }
+  
 end
