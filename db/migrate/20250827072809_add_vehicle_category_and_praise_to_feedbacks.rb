77880ba778246f0a116150df7bb401fe55d92b98
NIKHIL <nikhil@NIKHILs-MacBook-Air.local>
2025-08-27 14:42:47 +0530

    commit

--- db/migrate/20250827072809_add_vehicle_category_and_praise_to_feedbacks.rb
+++
@@ -1,6 +0,0 @@
-class AddVehicleCategoryAndPraiseToFeedbacks < ActiveRecord::Migration[7.1]
-  def change
-    add_column :feedbacks, :vehicle_category, :string
-    add_column :feedbacks, :praise_note, :text
-  end
-end
