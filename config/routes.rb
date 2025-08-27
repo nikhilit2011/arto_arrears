0e12c672eb52b0e0eedd8e61eb2e8e883e168563
NIKHIL <nikhil@NIKHILs-MacBook-Air.local>
2025-08-27 15:04:32 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -21,23 +21,5 @@ Rails.application.routes.draw do
     end
   end
 
-  # === TEMP ADMIN CREATOR ROUTE (remove after first use) ===
-  get "/create_admin", to: proc { |env|
-    # hardcoded one-time credentials
-    email    = "admin@example.com"
-    password = "supersecurepassword"
-
-    user = User.find_or_initialize_by(email: email)
-    user.password = password
-    user.password_confirmation = password
-    user.role = "admin" if user.respond_to?(:role=)
-    user.save!
-
-    [
-      200,
-      { "Content-Type" => "text/plain" },
-      ["Admin created/updated. Email: #{email}, Password: #{password}"]
-    ]
-  }
-  # === END TEMP ROUTE ===
+ 
 end
